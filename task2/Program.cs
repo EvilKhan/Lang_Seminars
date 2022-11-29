2cd19b1ba638aa0107f2af25b05ddaa57be18a8f
UsalTatar <barbariandiii@gmail.com>
2022-11-29 19:42:58 +0700

    task 2 complited

---
+++ task2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int m = int.Parse(Console.ReadLine());
+if (n > m)
+    Console.WriteLine($"max: {n}, min: {m}");
+else if (m > n)
+    Console.WriteLine($"max: {m}, min: {n}");
+else
+    Console.WriteLine($"{n} = {m}");
