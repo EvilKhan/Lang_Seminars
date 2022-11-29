c272fc2447d3cfb21ec96aad045b549c376712f3
UsalTatar <barbariandiii@gmail.com>
2022-11-29 21:05:04 +0700

    доавил еще один вариант решения

--- task8/Program.cs
+++ task8/Program.cs
@@ -6,7 +6,13 @@ Console.Clear();
 Console.Write("Введите число: ");
 int n = int.Parse(Console.ReadLine());
 int i = 2;
-while (i < n)
-{    Console.Write($"{i} ");
+/*while (i <= n)
+{    Console.Write($"{i} ");     //оба варианта работают, этот самый очевидный
      i = i + 2;
+}*/
+while(i <= n)
+{    
+    if (i % 2 == 0)
+      Console.Write($"{i} ");
+    i++;
 }
