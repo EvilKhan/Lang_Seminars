15eb6991c64052253763fe04b91bea6e98da035e
UsalTatar <barbariandiii@gmail.com>
2022-11-29 20:12:20 +0700

    task 4 complited

---
+++ task4/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+Console.Write("Введите число 1: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите число 2: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите число 3: ");
+int l = int.Parse(Console.ReadLine());
+int max = n;
+if (n > max)
+    max = n;
+else if (m > max)
+    max = m;
+else if (l > max)
+    max = l;
+Console.WriteLine($"{n}, {m}, {l} -> max: {max}");
